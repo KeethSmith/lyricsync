bb5622def2b12a2739511af552c14007f1352a45
KeethSmith <keethsmith@ufl.edu>
2026-09-06 03:00:45 -0400

    Configure Spotify client for direct sign-in

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Public Spotify app identifier, shared by all visitors. Never put a client secret here.
-export const SPOTIFY_CLIENT_ID = '';
+export const SPOTIFY_CLIENT_ID = 'c109941666994eb9bc521b952bc2abd6';
